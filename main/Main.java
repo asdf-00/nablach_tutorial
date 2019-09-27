8c893543654b537979020e7e5c14d24bc067280d
tiger00000555 <tiger00000555@yahoo.co.jp>
2019-09-28 00:26:16 +0900

    00

--- main/Main.java
+++ main/Main.java
@@ -1,31 +1,10 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-//		System.out.println("組数を入力");
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
 
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("各組に数を入力");
-//		String a = bf.readLine();
-		int b = Integer.parseInt(bf.readLine());
-		int[] n= new int[b];
-		try {
-			
-			for(int i=1; i<=n.length; i++) {
-				
-				String c = bf.readLine();
-//				n[i] =  bf.readLine();
-//				System.out.println(c);
-			}
-		} catch (Exception e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
-			
 	}
+
 }
